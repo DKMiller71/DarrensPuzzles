b8b49717ee1eb865b576252e4d5dbdabb216725e
DKMiller71 <45632749+DKMiller71@users.noreply.github.com>
2022-10-14 12:11:44 -0400

    Cryptic2022-10

--- puzzles/xword/exolve/dkm-decode.js
+++ puzzles/xword/exolve/dkm-decode.js
@@ -14,17 +14,21 @@ function decryptStringWithXOR(input,key) {
 	return (new TextDecoder()).decode(c);
 }
 
-let params = new URLSearchParams(document.location.search);
-let query = params.get('key');
+function initDecode() {
+	let params = new URLSearchParams(document.location.search);
+	let query = params.get('key');
 
-if(query && typeof(exolvedata) !== 'undefined') {
-	
-	let key = query instanceof Array ? query[0] : query;
-	exdata = decryptStringWithXOR(exolvedata, key);
-	
-	if(exdata.match(/[^\s\x20-\x7E]/)) {
-		// bad characters or key didn't match
-	} else if(exdata.match(/exolve/)) {
-		createExolve(exdata);		
-	} 
+	if(query && typeof(exolvedata) !== 'undefined') {
+		
+		let key = query instanceof Array ? query[0] : query;
+		exdata = decryptStringWithXOR(exolvedata, key);
+		
+		if(exdata.match(/[^\s\x20-\x7E]/)) {
+			// bad characters or key didn't match
+		} else if(exdata.match(/exolve/)) {
+			createExolve(exdata);		
+		} 
+	}
 }
+
+window.onload = initDecode();
